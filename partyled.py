e85225005dc77bacb31d84e496be6572acbdb772
Tomas Harkema <tomas@harkema.in>
2015-10-07 18:36:44 +0200

    remove inefficient code. use side-effects

--- partyled.py
+++ partyled.py
@@ -19,14 +19,18 @@ except ImportError, e:
 
 STRIPCOUNT = 10  # number of Q42 awesome 12V analog RGB LED strips. 10 is the max for now.
 PWMSCALE = 4096  # fit in PWM bitdepth. PCA9685 has a 12-bit PWM converter.
-GAMMA = 2.2      # gamma correction
+GAMMA = 2.2  # gamma correction
+
+colors = [0] * STRIPCOUNT * 3
+
 
 def setupPWM():
-    pwm1 = PWM(0x40) # PCA9685 board one
-    pwm2 = PWM(0x41) # PCA9685 board two
-    pwm1.setPWMFreq(100) # Not too low, to keep responsiveness to signals high
-    pwm2.setPWMFreq(100) # Also not too high, to prevent voltage rise to cut off and reduce brightness
+    pwm1 = PWM(0x40)  # PCA9685 board one
+    pwm2 = PWM(0x41)  # PCA9685 board two
+    pwm1.setPWMFreq(100)  # Not too low, to keep responsiveness to signals high
+    pwm2.setPWMFreq(100)  # Also not too high, to prevent voltage rise to cut off and reduce brightness
     return (pwm1, pwm2)
+
 
 if (PWMAvailable):
     (pwm1, pwm2) = setupPWM()
@@ -36,12 +40,12 @@ frames = 0
 fps = 0
 fpstimer = 0
 
+
 # set a single strip's color.
 #   StripID is 0..STRIPCOUNT
 #   r, g, b is 0..1
 
 def setStripColor(stripID, r, g, b):
-
     if (PWMAvailable):
         if stripID < 5:
             pwm1.setPWM(stripID * 3 + 0, 0, pwmscale(r))
@@ -55,10 +59,11 @@ def setStripColor(stripID, r, g, b):
 
 # clip and scale a 0..1 input (inclusive) to 0..PWMSCALE
 def pwmscale(val):
-  p = (val ** GAMMA) * PWMSCALE;
-  if p < 0: p = 0;
-  if p > PWMSCALE-1: p = PWMSCALE-1
-  return int(p)
+    p = (val ** GAMMA) * PWMSCALE;
+    if p < 0: p = 0;
+    if p > PWMSCALE - 1: p = PWMSCALE - 1
+    return int(p)
+
 
 # --------------- GENERATORS ------------------------------
 # these actually drive the LEDs. TODO make these into actual plugins
@@ -73,52 +78,50 @@ def pwmscale(val):
 # generator: fast/hard on/off walk
 # example of when you might want to use the framecounter
 def generator_Strobe(dT, fr, sC):
-    colors = [0] * STRIPCOUNT * 3
+    global colors
     for i in range(0, sC):
         c = 0
         if fr % 10 == i: c = 1
-        colors[i*3 + 0] = c
-        colors[i*3 + 1] = c
-        colors[i*3 + 2] = c
+        colors[i * 3 + 0] += c
+        colors[i * 3 + 1] += c
+        colors[i * 3 + 2] += c
 
-    return colors
 
 # generator: smooth grayscale sinewave across strips
 def generator_Wave(dT, fr, sC):
-    colors = [0] * STRIPCOUNT * 3
+    global colors
     for i in range(0, sC):
-        colors[i*3 + 0] = 0.5 + 0.5 * math.sin(dT * 5.2 + i * 1.6)
-        colors[i*3 + 1] = 0.5 + 0.5 * math.sin(dT * 5.4 + i * 1.6)
-        colors[i*3 + 2] = 0.5 + 0.5 * math.sin(dT * 5.6 + i * 1.6)
+        colors[i * 3 + 0] += 0.5 + 0.5 * math.sin(dT * 5.2 + i * 1.6)
+        colors[i * 3 + 1] += 0.5 + 0.5 * math.sin(dT * 5.4 + i * 1.6)
+        colors[i * 3 + 2] += 0.5 + 0.5 * math.sin(dT * 5.6 + i * 1.6)
 
     return colors
+
 
 def generator_Wave_Green(dT, fr, sC):
-    colors = [0] * STRIPCOUNT * 3
+    global colors
     for i in range(0, sC):
-        colors[i*3 + 0] = 0
-        colors[i*3 + 1] = 0.5 + 0.5 * math.sin(dT * 5.4 + i * 1.6)
-        colors[i*3 + 2] = 0
+        colors[i * 3 + 1] += 0.5 + 0.5 * math.sin(dT * 5.4 + i * 1.6)
 
     return colors
 
+
 def generator_ON(dT, fr, sC):
-  for i in range(0, sC * 3):
-    colors[i] = 1
+    for i in range(0, sC * 3):
+        colors[i] = 1
+
 
 amp = 1
 
 generators = []
 
+
 def tick():
+    global colors
     colors = [0] * STRIPCOUNT * 3
-
     for generator in generators:
-        newColors = generator(time.time(), frames, STRIPCOUNT)
-        for i in range(0, len(colors)):
-            colors[i] = colors[i] + newColors[i]
+        generator(time.time(), frames, STRIPCOUNT)
 
-    return colors
 
 class LightsThread(threading.Thread):
     def run(self):
@@ -126,24 +129,31 @@ class LightsThread(threading.Thread):
         global frames
         global fpstimer
 
-        while (True):
+        r = 0
+        g = 0
+        b = 0
 
-            colors = tick()
+        while True:
+            global colors, r, g, b
+            time.sleep(0.001)
+            tick()
+
             string = ""
-
             for i in range(0, STRIPCOUNT):
-                r = colors[i*3] * amp
-                g = colors[i*3 + 1] * amp
-                b = colors[i*3 + 2] * amp
+                r = colors[i * 3] * amp
+                g = colors[i * 3 + 1] * amp
+                b = colors[i * 3 + 2] * amp
 
                 setStripColor(i, r, g, b)
-                string = string + "0,"+str(i)+","+str(r)+","+str(g)+","+str(b)+";"
+                string = string + "0," + str(i) + "," + str(r) + "," + str(g) + "," + str(b) + ";"
+
+            # print string for capture with node
             print string
 
             fps += 1
             frames += 1
             if time.time() > fpstimer + 1.0:
-                print "1,FPS: ",fps,";"
+                print "1,FPS: ", fps, ";"
                 fps = 0
                 fpstimer = time.time()
 
@@ -151,7 +161,7 @@ class LightsThread(threading.Thread):
 class InputThread(threading.Thread):
     def run(self):
         global generators
-        while(True):
+        while True:
             string = raw_input()
 
             setGenerators = string.split("%")
@@ -159,17 +169,18 @@ class InputThread(threading.Thread):
             newGenerator = []
 
             for i in range(0, len(setGenerators)):
-                if (len(setGenerators[i]) > 1):
+                if len(setGenerators[i]) > 1:
                     switch = setGenerators[i].split("$")
                     name = switch[0]
-                    if (name == "wavegreen" and switch[1] == '1'):
+                    if name == "wavegreen" and switch[1] == '1':
                         newGenerator.append(generator_Wave_Green)
-                    if (name == "strobe" and switch[1] == '1'):
+                    if name == "strobe" and switch[1] == '1':
                         newGenerator.append(generator_Strobe)
-                    if (name == "wavecolor" and switch[1] == '1'):
+                    if name == "wavecolor" and switch[1] == '1':
                         newGenerator.append(generator_Wave)
 
             generators = newGenerator
+
 
 lightsThread = LightsThread()
 lightsThread.start()
