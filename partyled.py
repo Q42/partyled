f8daf0a4e052530aba7f915de93d0c307eee7366
Tomas Harkema <tomas@harkema.in>
2015-10-13 20:39:21 +0200

    fix remote host

--- partyled.py
+++ partyled.py
@@ -224,7 +224,7 @@ class AppThread(threading.Thread):
             return jsonify(generatorsByName)
 
         if __name__ == '__main__':
-            app.run(debug=True, use_reloader=False, threaded=True)
+            app.run(debug=True, use_reloader=False, threaded=True, host='0.0.0.0')
 
 appThread = AppThread()
 try:
