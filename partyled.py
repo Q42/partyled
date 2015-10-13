0883479db411c8347a96f2cc6af6649360cbf2f5
Tomas Harkema <tomas@harkema.in>
2015-10-13 20:28:49 +0200

    fix python webserver

--- partyled.py
+++ partyled.py
@@ -5,12 +5,11 @@ import threading
 import time
 import math
 import sys
-import platform
-import os
-import socket
-import fileinput
+from flask import Flask, render_template, jsonify
 
 PWMAvailable = True
+
+
 
 try:
     from Adafruit_PWM_Servo_Driver import PWM
@@ -22,7 +21,6 @@ PWMSCALE = 4096  # fit in PWM bitdepth. PCA9685 has a 12-bit PWM converter.
 GAMMA = 2.2  # gamma correction
 
 colors = [0] * STRIPCOUNT * 3
-
 
 def setupPWM():
     pwm1 = PWM(0x40)  # PCA9685 board one
@@ -145,8 +143,9 @@ def generator_Green_Burst(dT, fr, sC):
 amp = 1
 
 generators = []
-
+generatorsByName = {}
 currentTime = time.time()
+
 def tick():
     global colors
     colors = [0] * STRIPCOUNT * 3
@@ -156,20 +155,6 @@ def tick():
 
     generator_Green_Burst(currentTime, frames, STRIPCOUNT)
 
-def sleepFromFPS(currentFps):
-	return
-	# if fps < 60:
-	# 	return
-
-	# fw = (1.0 / 60)
-	# fc = (1.0 / currentFps)
-
-	# sleepTime = fw - fc
-
-	# print "1,FPS sleep: ", fw, fc, currentFps, sleepTime, ";"
-
-	# time.sleep(0.001)
-
 class LightsThread(threading.Thread):
     def run(self):
         global fps, frames, fpstimer, r, g, b
@@ -177,72 +162,79 @@ class LightsThread(threading.Thread):
         r = 0
         g = 0
         b = 0
-        currentFPS = 0
 
         while True:
             global colors, r, g, b
             tick()
 
-            string = ""
             for i in range(0, STRIPCOUNT):
                 r = colors[i * 3] * amp
                 g = colors[i * 3 + 1] * amp
                 b = colors[i * 3 + 2] * amp
 
                 setStripColor(i, r, g, b)
-                string = string + "0," + str(i) + "," + str(r) + "," + str(g) + "," + str(b) + ";"
-
-            # print string for capture with node
-            print string
-
-            sleepFromFPS(fps)
 
             fps += 1
             frames += 1
             if time.time() > fpstimer + 1.0:
-            	currentFPS = fps
                 print "1,FPS: ", fps, ";"
                 fps = 0
                 fpstimer = time.time()
 
+def updateGenerators():
+    global generators
+    global generatorsByName
+    newGenerator = []
+    for name, value in generatorsByName.iteritems():
+        print name, value
+        if name == "wavegreen" and value == 1:
+            newGenerator.append(generator_Wave_Green)
+        if name == "waveblue" and value == 1:
+            newGenerator.append(generator_Wave_Blue)
+        if name == "wavepurple" and value == 1:
+            newGenerator.append(generator_Wave_Purple)
+        if name == "wavered" and value == 1:
+            newGenerator.append(generator_Wave_Red)
+        if name == "wavewhite" and value == 1:
+            newGenerator.append(generator_Wave_White)
+        if name == "scatter" and value == 1:
+            newGenerator.append(generator_Scatter)
+        if name == "strobe" and value == 1:
+            newGenerator.append(generator_Strobe)
+        if name == "wavecolor" and value == 1:
+            newGenerator.append(generator_Wave)
 
-class InputThread(threading.Thread):
+    print newGenerator
+
+    generators = newGenerator
+
+
+class AppThread(threading.Thread):
     def run(self):
-        global generators
-        while True:
-            string = raw_input()
-            if string == "e":
-            	process.exit()
+        app = Flask(__name__)
 
-            setGenerators = string.split("%")
+        @app.route("/")
+        def hello():
+            return render_template('index.html')
 
-            newGenerator = []
+        @app.route("/generator/<string:name>/<int:value>")
+        def generator(name, value):
+            generatorsByName[name] = value
+            updateGenerators()
+            return jsonify(generatorsByName)
 
-            for i in range(0, len(setGenerators)):
-                if len(setGenerators[i]) > 1:
-                    switch = setGenerators[i].split("$")
-                    name = switch[0]
-                    if name == "wavegreen" and switch[1] == '1':
-                        newGenerator.append(generator_Wave_Green)
-                    if name == "waveblue" and switch[1] == '1':
-                        newGenerator.append(generator_Wave_Blue)
-                    if name == "wavepurple" and switch[1] == '1':
-                        newGenerator.append(generator_Wave_Purple)
-                    if name == "wavered" and switch[1] == '1':
-                        newGenerator.append(generator_Wave_Red)
-                    if name == "wavewhite" and switch[1] == '1':
-                        newGenerator.append(generator_Wave_White)
-                    if name == "scatter" and switch[1] == '1':
-                        newGenerator.append(generator_Scatter)
-                    if name == "strobe" and switch[1] == '1':
-                        newGenerator.append(generator_Strobe)
-                    if name == "wavecolor" and switch[1] == '1':
-                        newGenerator.append(generator_Wave)
+        if __name__ == '__main__':
+            app.run(debug=True, use_reloader=False, threaded=True)
 
-            generators = newGenerator
+appThread = AppThread()
+try:
+    appThread.setDaemon(True)
+    appThread.start()
 
+    lightsThread = LightsThread()
+    lightsThread.run()
+except KeyboardInterrupt:
+    print "Ctrl-c pressed ..."
+    appThread.join(0)
+    sys.exit()
 
-lightsThread = LightsThread()
-lightsThread.start()
-inputThread = InputThread()
-inputThread.start()
